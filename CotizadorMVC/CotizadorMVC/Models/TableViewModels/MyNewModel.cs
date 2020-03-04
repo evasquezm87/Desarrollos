50ee51578f199718809fa5e87f93bb28e36eb02d
evasquezm87 <evasquezm87@gmail.com>
2020-03-04 17:11:50 -0600

    prueba desde vs

---
+++ CotizadorMVC/CotizadorMVC/Models/TableViewModels/MyNewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CotizadorMVC.Models.TableViewModels
+{
+    public class MyNewModel
+    {
+        string prueba;
+        string repositorio;
+    }
+}
